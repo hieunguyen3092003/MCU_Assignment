3750e25fd84c7f8215fa5fed3cb96b355364e1ec
Viet Bach <vbachh.bui1611@gmail.com>
2023-12-08 00:26:43 +0700

    Update UART.h

--- STM32/Core/Inc/UART.h
+++ STM32/Core/Inc/UART.h
@@ -1,20 +1,23 @@
 /*
  * UART.h
  *
- *  Created on: Dec 7, 2023
+ *  Created on: Dec 8, 2023
  *      Author: bach
  */
+
+
 
 #ifndef INC_UART_H_
 #define INC_UART_H_
 
+
 #include"stm32f1xx_hal.h"
 #include <stdio.h>
 
-void uart_SendMode(uint8_t mode);
 void uart_Init(void);
-void uart_SendTimeTraffic(uint8_t id, uint16_t number);
-void uart_SendBufferRed(uint16_t number);
+void UARTSendMode(uint8_t mode);
+void UARTCountdownTrafficTime(uint8_t id,uint16_t time);
+void UARTSendBufferRed(uint16_t number) ;
 void uart_SendBufferGreen(uint16_t number);
 void uart_SendBufferYellow(uint16_t number);
 
