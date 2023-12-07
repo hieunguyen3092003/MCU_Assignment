f21baf19c0d85709da454f444ab6ada53dd74dc7
Viet Bach <vbachh.bui1611@gmail.com>
2023-12-08 00:29:40 +0700

    Create softwareTimer.h

---
+++ STM32/Core/Inc/softwareTimer.h
@@ -0,0 +1,29 @@
+/*
+ * softwareTimer.h
+ *
+ *  Created on: Dec 8, 2023
+ *      Author: bach
+ */
+
+#ifndef INC_SOFTWARETIMER_H_
+#define INC_SOFTWARETIMER_H_
+
+int timer1_counter;
+extern int timer1_flag;
+void setTimer1();
+
+void timerRun();
+
+int timer2_counter;
+extern int timer2_flag;
+void setTimer2();
+
+int timer3_counter;
+extern int timer3_flag;
+void setTimer3();
+
+int timer4_counter;
+extern int timer4_flag;
+void setTimer4();
+
+#endif /* INC_SOFTWARETIMER_H_ */
